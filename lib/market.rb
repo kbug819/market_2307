20851997d20af552237e7930e654c44788bd6a56
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-08 11:02:06 -0500

    vendors that sell - market

---
+++ lib/market.rb
@@ -0,0 +1,28 @@
+class Market
+  attr_reader :name, :vendors
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    list = []
+    @vendors.each do |vendor|
+      if vendor.inventory.include?(item)
+        list << vendor
+      end
+    end
+    list
+  end
+    
+end
